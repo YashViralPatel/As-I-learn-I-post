8e8fb4ca4bc1c2900f7e41fec6cd6780bea18bd1
YashViralPatel <113464431+YashViralPatel@users.noreply.github.com>
2022-09-14 12:30:03 +0530

    Create Area of Square.c
    This is a program to calculate the area of a square when the side is given by the user.

---
+++ Area of Square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+// This programme is to calculate the area of a square when the side of the square is given. 
+int main() {
+float side;
+
+printf("Enter the side of square in centimeters  ");
+scanf("%f", &side);
+
+printf("The area of the square is %f", side * side);
+
+return 0;
+}
+// same programme can also be used to square a number.
