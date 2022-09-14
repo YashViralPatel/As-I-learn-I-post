b491d4c1b8c8459b2248c47411ffc2311f3f5228
Yash Patel <yashviralpatel@gmail.com>
2022-09-14 12:37:36 +0530

    Create cube of a number n.c

---
+++ cube of a number n.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// This programme is to print the cube of a number n provided by the user.
+int main () {
+float n;
+
+printf("Enter value of number n  ");
+scanf("%f", &n);
+
+printf("The cube of the number n is %f", n * n * n);
+
+return 0;
+}
